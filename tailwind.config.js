d642d6f1546e822d857299330c5a073c1e82612d
SIFAT69 <sifatkazi69@gmail.com>
2022-10-07 04:36:31 +0600

    design korlam ekhono onek khala baki ekhane

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
     ],
 
     theme: {
+        container: {
+          center:true,
+        },
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
